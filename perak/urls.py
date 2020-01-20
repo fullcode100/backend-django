7a267c2c93d36f51df262208b3bdd7069d71425c
fullcode100 <snowleopard722@outlook.com>
2020-01-20 18:07:02 +0700

    Tweak settings.py

--- perak/urls.py
+++ perak/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from match import match_routers
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('api/', include(match_routers.router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
