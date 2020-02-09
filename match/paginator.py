e7779fa5e1f0ef9d87919fb85508e9016692c364
fullcode100 <snowleopard722@outlook.com>
2020-02-09 10:11:50 +0700

    Override get_paginated_response function

--- match/paginator.py
+++ match/paginator.py
@@ -1,7 +1,16 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MatchHistoryPaginator(PageNumberPagination):
     page_size = 10
     page_size_query_param = "size"
     page_query_param = "page"
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link().replace("", ""),  # old,new
+            'previous': self.get_previous_link().replace("", ""),  # old,new
+            'count':self.page.paginator.count,
+            'result':data
+        })
